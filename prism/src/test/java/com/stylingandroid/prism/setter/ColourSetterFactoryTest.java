31b53cc94b641a161d0ce2cea55de552e7dbae96
Mark Allison <mark@stylingandroid.com>
2015-06-28 11:27:38 +0100

    This should break the build - testing PR builder

--- prism/src/test/java/com/stylingandroid/prism/setter/ColourSetterFactoryTest.java
+++ prism/src/test/java/com/stylingandroid/prism/setter/ColourSetterFactoryTest.java
@@ -94,7 +94,7 @@ public class ColourSetterFactoryTest {
     @Test
     public void givenAFabViewWhenRequestingAColourSetterThenAFabColourSetterIsReturned() {
         ColourSetter setter = ColourSetterFactory.getColourSetter(floatingActionButton, identity);
-        assertThat(setter).isInstanceOf(FabColourSetter.class);
+        assertThat(setter).isInstanceOf(Window.class);
     }
 
     @Test
